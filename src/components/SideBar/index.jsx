789983c0699a3ad65add97c20db14024af1f7a8d
incredible92 <sulymanabdullahi@gmail.com>
2021-06-09 14:31:08 +0100

    update: sideBar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -7,9 +7,46 @@ const sideBar = () => {
       <div className="sideBar">
         <img className="logo" src="/images/logo.svg" alt="logo" />
         <p>Main pages</p>
-        <div className="dash">
+        <div className="dash flex">
           <img className="icon" src="/images/Category.svg" alt="dash" />
           <span className="space">Dashboard</span>
+        </div>
+
+        <div className="wallet flex">
+          <img className="icon" src="/images/Wallet.svg" alt="wallet" />
+          <span className="space">Balances</span>
+        </div>
+
+        <div className="customer flex">
+          <img className="icon" src="/images/3 User.svg" alt="user" />
+          <span className="space">Customer</span>
+        </div>
+
+        <div className="act flex">
+          <img className="icon" src="/images/Activity.svg" alt="act" />
+          <span className="space">Analytics</span>
+        </div>
+
+        <p>General</p>
+
+        <div className="setting flex">
+          <img className="icon" src="/images/Filter.svg" alt="setting" />
+          <span className="space">Settings</span>
+        </div>
+
+        <div className="team flex">
+          <img className="icon" src="/images/Star.svg" alt="team" />
+          <span className="space">Team</span>
+        </div>
+
+        <div className="contact flex">
+          <img className="icon" src="/images/Call.svg" alt="contact" />
+          <span className="space">Contact</span>
+        </div>
+
+        <div className="log flex">
+          <img className="icon" src="/images/Logout.svg" alt="log" />
+          <span className="space">Logout</span>
         </div>
       </div>
     </>
